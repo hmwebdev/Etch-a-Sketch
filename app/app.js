999894e7981bee6a507c12db471d387a3e8e12c2
hmwebdev <non_phixion418@hotmail.com>
2024-02-18 16:28:23 -0500

    Add graphic background and colors modification

--- app/app.js
+++ app/app.js
@@ -1,15 +1,18 @@
 // Globals Var
 const gridContainer = document.querySelector(".container");
+const button = document.querySelector("#add");
 
 // Functions
 
-function createGrid() {
-  for (let i = 1; i <= 16; i++) {
+function buildGrid(width = 16, height = 16) {
+  let container = document.createElement("div");
+  container.className = "container";
+  for (let i = 1; i <= width; i++) {
     let row = document.createElement("div");
     gridContainer.appendChild(row);
     row.className = "row";
 
-    for (let j = 1; j <= 16; j++) {
+    for (let j = 1; j <= height; j++) {
       let square = document.createElement("div");
       row.appendChild(square);
       square.className = "square";
@@ -17,12 +20,38 @@ function createGrid() {
   }
 }
 
-createGrid();
+function sketch() {
+  const squareSelector = document.querySelectorAll(".square");
+  squareSelector.forEach((square) => {
+    square.addEventListener("mouseenter", () => {
+      square.style.backgroundColor = "black";
+    });
+  });
+}
+
+buildGrid();
+sketch();
 
 // Watcher
-const squareSelector = document.querySelectorAll(".square");
-squareSelector.forEach((square) => {
-  square.addEventListener("mouseenter", () => {
-    square.style.backgroundColor = "red";
-  });
+
+button.addEventListener("click", () => {
+  let width = prompt("Enter a number for the width");
+  let height = prompt("Enter a number for the height");
+  if (isNaN(width) === false && isNaN(height) === false) {
+    if (width > 1 && width <= 100 && height > 1 && height <= 100) {
+      gridContainer.replaceChildren();
+      buildGrid(width, height);
+      sketch();
+    } else if (!width || !height) {
+      gridContainer.replaceChildren();
+      buildGrid();
+      sketch();
+    } else {
+      alert(
+        "Please enter a number between 2 and 100. Or empty for the default grid"
+      );
+    }
+  } else {
+    alert("Please enter a number only. Max 100");
+  }
 });
